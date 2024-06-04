c0214f9b81a757108c41891c19a6e1ae82794a76
Maksym Pryhoda <maksym.pryhoda@sysgears.com>
2024-06-04 16:16:45 +0300

    Reorganize structure

--- UniversityStuff-master/5 sem/OOP/Lab1_OOP/src/main/java/com/music_edu/main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.music_edu.main;
-
-import com.music_edu.container.Disk;
-
-public class Main {
-    public static void main(String[] args){
-        Disk disk = new Disk();
-    }
-}
--- UniversityStuff-master/5 sem/OOP/Lab1_OOP/src/main/java/com/music_edu/music/MusicGenre.java
+++
@@ -1,3 +0,0 @@
-package com.music_edu.music;
-
-public enum MusicGenre {POP, ROCK, HIPHOP, RAP,  RandB,  JAZZ, INSTRUMENTAL, FOLK, ELECTRO, TRANS, POP_ROCK, ALTERNATIVE_ROCK;}
